796c88b34de8b497cfce863c1d151ba506455374
Gabriela <gabrielacamarchiori@gmail.com>
2023-04-14 16:43:55 -0300

    add: component header

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+    colors: {
+        brand: {
+            pink1: "#a54086",
+            pink2: "#bc589f",
+            pink3: "#ffa1e8"
+        },
+        secundary: {
+            grey1: "#080a07",
+            grey2: "#10130d",
+            grey3: "#d9d6dc",
+            grey4: "#ecebee"
+        }
+
+    },
+    
+    
+    
+})
+
+export default theme
